8f3dcb86ccd3fc544c94ebe958f675623b31112e
Mohamed Ahmed Mufreh <93543045+mohamedmufreh@users.noreply.github.com>
2022-04-02 00:00:04 +0200

    Update watermark.m

--- watermark.m
+++ watermark.m
@@ -3,9 +3,8 @@ function Iw = watermark(O,W)  % O for original image and W for watermark image
 
 [M,N] = size(O);                                   % getting the size of the original image
 I=rgb2gray(O);                                      % converting rgb image to gray image
-W_BW = im2bw(W);         % converting the watermark  image to be in (0&1) bits
-W_resized = imresize(W_BW,[M,N]);  % resizing the watermark image to be as the original image     
-Iw = bitset (I,1,W_resized);                   % Now we replace the lowest bit in the original image using bitset function 
+W_BW = im2bw(W);         % converting the watermark  image to be in (0&1) bits     
+Iw = bitset (I,1,W_BW);                   % Now we replace the lowest bit in the original image using bitset function 
   %%%%%%%%%%%%%%%%%  
 figure,imshow(O),title('Original Image')
 figure,imshow(W),title('Watermark Image')
