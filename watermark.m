ccd0004a49e64fca7892463fe4651ecc2b63cee3
Mohamed Ahmed Mufreh <93543045+mohamedmufreh@users.noreply.github.com>
2022-04-01 23:49:06 +0200

    Add files via upload

---
+++ watermark.m
@@ -0,0 +1,12 @@
+function Iw = watermark(O,W)  % O for original image and W for watermark image
+% Both image must have the same size
+
+[M,N] = size(O);                                   % getting the size of the original image
+I=rgb2gray(O);                                      % converting rgb image to gray image
+W_BW = im2bw(W);         % converting the watermark  image to be in (0&1) bits
+W_resized = imresize(W_BW,[M,N]);  % resizing the watermark image to be as the original image     
+Iw = bitset (I,1,W_resized);                   % Now we replace the lowest bit in the original image using bitset function 
+  %%%%%%%%%%%%%%%%%  
+figure,imshow(O),title('Original Image')
+figure,imshow(W),title('Watermark Image')
+figure,imshow(Iw),title('Result')
